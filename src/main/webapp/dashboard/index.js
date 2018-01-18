b2d44bd0ffd303f1adfe7a9a61bb744d7430b2bb
Miguel Angel Fernandez <elmendalerenda@gmail.com>
2018-01-18 15:56:41 +0100

    add header to activity stream

--- src/main/webapp/dashboard/index.js
+++ src/main/webapp/dashboard/index.js
@@ -6,8 +6,15 @@ Vue.component('activities', {
     return { currentOrder: 'project' }
   },
   computed : {
-    actis: function() {
-      return this.activities.sort((a, b) => { return b[this.currentOrder] > a[this.currentOrder];});
+    groupedActivities: function() {
+      var criteria = this.currentOrder
+
+      return this.activities.reduce(function(groups, item) {
+        var val = item[criteria];
+        groups[val] = groups[val] || [];
+        groups[val].push(item);
+        return groups;
+      }, {});
     }
   },
   methods: {
@@ -19,11 +26,17 @@ Vue.component('activities', {
   <div>
     <button v-on:click="orderBy('date')">Order by date</button>
     <button v-on:click="orderBy('project')">Order by project</button>
+
     <ul>
-      <li v-for="activity in actis"  v-bind:class="{ newProject: activity.project, newUser: activity.user }">
-        <article>
-          <section>{{activity.formattedType}} card <span>{{activity.cardName}}</span> by {{activity.user}} @ {{activity.formattedDate}}</section>
-        </article>
+      <li v-for="(activities, grouping) in groupedActivities">
+        <span>{{grouping}} </span>
+        <ul>
+          <li v-for="activity in activities"  v-bind:class="{ newProject: activity.project, newUser: activity.user }">
+            <article>
+              <section>{{activity.formattedType}} card <span>{{activity.cardName}}</span> by {{activity.user}} @ {{activity.formattedDate}}</section>
+            </article>
+          </li>
+        </ul>
       </li>
     </ul>
   </div>
