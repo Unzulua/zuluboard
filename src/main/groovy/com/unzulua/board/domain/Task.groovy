8c827e4bb1c7a7e6e4228557511e063b0e6bc22f
Miguel Angel Fernandez <elmendalerenda@gmail.com>
2018-01-19 17:07:03 +0100

    move date format to domain

--- src/main/groovy/com/unzulua/board/domain/Task.groovy
+++ src/main/groovy/com/unzulua/board/domain/Task.groovy
@@ -6,6 +6,7 @@ import grails.trello.domain.BoardList
 class Task {
     String description
     Date dueDate
+    String formattedDate
     String cardId
     String projectName
     String idList
@@ -13,12 +14,17 @@ class Task {
 
     static Task from(Card card) {
         Task task = new Task(cardId: card.id, dueDate: card.dueDate, idList: card.idList, labels: card.labels)
+        task.formattedDate = formatDate(card.dueDate)
         task.description = card.name
         task.projectName = ""
         if (card.description){
             task.description += " - ${card.description}"
         }
         task
+    }
+
+    private static formatDate(Date date){
+        date.format('dd-MM-yyyy hh:mm')
     }
 
     void setProject(BoardList list){
